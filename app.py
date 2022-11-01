11055cf572f3a1700e0a0c409ab00e725e061451
백재승 <notice6143@kau.kr>
2022-11-01 10:36:00 +0000

    model api

--- app.py
+++ app.py
@@ -7,12 +7,12 @@ import numpy as np
 app = flask.Flask(__name__)
 model = None
 
-
+#model load
 def load_model():
     global model
-    model = tensorflow.keras.models.load_model('/workspace/DataflowTest/DataflowModel.h5')
+    model = tensorflow.keras.models.load_model('/workspace/DataflowModel/DataflowModel.h5')
 
-
+#http://ip:port/api/predict -> skill
 @app.route("/api/predict", methods=["POST"])
 def api_predict():
 
@@ -30,11 +30,11 @@ def api_predict():
         image = ImageOps.fit(image, size, Image.ANTIALIAS) #방향값 제거, 안티앨리어싱
         image_array = np.asarray(image)
         normalized_image_array = (image_array.astype(np.float32) / 255.0)	#normalizing
-        data = np.ndarray(shape=(1, 150, 150, 3), dtype=np.float32)
+        data = np.ndarray(shape=(1, 150, 150, 3), dtype=np.float32)	#reshape
         data[0] = normalized_image_array
         prediction = model.predict(data)	#예측
 
-        output = np.argmax(prediction, axis=-1)	#max
+        output = np.argmax(prediction, axis=-1)	#가장 높은 예측값
         
         msg = str(output)
         msg = msg + "단계입니다"
