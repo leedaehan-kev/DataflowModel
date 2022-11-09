ebb4baf08d3c86ffea2c37d3f471f1b93fc711b5
백재승 <notice6143@kau.kr>
2022-11-09 17:31:50 +0000

    User session update

--- application.py
+++ application.py
@@ -1,8 +1,59 @@
 import flask
 import sys
+
 application = flask.Flask(__name__)
 
-val=[]
+users = {}
+
+@application.route("/api/user",methods=["POST"])
+def api_user():
+    global users
+    req=flask.request.get_json()
+    user_id = req['userRequest']['user']['id']
+    json = {}
+    json[user_id] = []
+    users.update(json)
+    print(users)
+    return req
+
+#key:val, value:필요한 값
+#value 가져오기
+#http://ip:port/api/val -> skill
+@application.route("/api/val",methods=["POST"])
+def api_val():
+    global users
+    req=flask.request.get_json()
+    user_id = req['userRequest']['user']['id']
+    users[user_id].append(req['action']['clientExtra']['val'])
+    print(users)
+    return req
+
+#결과 출력
+@application.route("/api/result",methods=["POST"])
+def api_result():
+    global users
+    req=flask.request.get_json()
+    user_id = req['userRequest']['user']['id']
+    msg=''
+    for i in users[user_id]:
+        msg+=i+'\n'
+    print(msg)
+    print(users)
+    users.pop(user_id)
+    res = {
+            "version": "2.0",
+            "template": {
+                "outputs": [
+                    {
+                        "simpleText": {
+                            "text": msg
+                        }
+                    }
+                ]
+            }
+        }
+    return flask.jsonify(res)
+
 
 #http://ip:port/api/hello -> skill
 @application.route("/api/hello",methods=["POST"])
@@ -24,45 +75,30 @@ def api_hello():
         }
     return flask.jsonify(res)
 
-#key:val, value:필요한 값
-#value 가져오기
-#http://ip:port/api/val -> skill
-@application.route("/api/val",methods=["POST"])
-def api_val():
-    global val
-    req=flask.request.get_json()
-    print(req)
-    val.append(req['action']['clientExtra']['val'])
-    print(req['action']['clientExtra']['val'])
-    return req
-
-#결과 출력
-@application.route("/api/result",methods=["POST"])
-def api_result():
-    global val
-    req=flask.request.get_json()
-    msg=''
-    for i in val:
-        msg+=i+'\n'
-    print(msg)
-    val=[]
-    res = {
-            "version": "2.0",
-            "template": {
-                "outputs": [
-                    {
-                        "simpleText": {
-                            "text": msg
-                        }
-                    }
-                ]
-            }
-        }
-    return flask.jsonify(res)
-
-
 if __name__ == "__main__":
     application.run(host='0.0.0.0')
+    
+''' #json 추출
+@application.route("/api/user",methods=["POST"])
+def api_user():
+    global val, user_count
+    req=flask.request.get_json()
+    json = {}
+    user_id = req['userRequest']['user']['id']
+    json[user_id] = []
+    json[user_id].append("test")
+    json[user_id].append("test2")
+    user_count += 1
+    print(json)
+    for i in json[user_id]:
+        print(i)
+    val.update(json)
+    print(val)
+    print(val[user_id])
+    val.pop(user_id)
+    print(val)
+    return req
+'''
     
 
 '''
